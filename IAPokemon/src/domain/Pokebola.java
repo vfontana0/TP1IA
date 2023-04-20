ca056e5e057c6a25cbcf7d6da9d9a09e1f116ca0
Valentin Fontana <valefontana15@gmail.com>
2023-04-20 15:47:30 -0300

    Cambiado grafo para que los vertices sean nodos

--- IAPokemon/src/domain/Pokebola.java
+++
@@ -1,7 +0,0 @@
-package domain;
-
-public class Pokebola {
-
-	Nodo posicion; //nodo en el que esta la pokebola
-	Double puntos; //puntos que gana el agente al juntarla.
-}
--- IAPokemon/src/domain/Pokemon.java
+++
@@ -1,9 +0,0 @@
-package domain;
-
-public class Pokemon {
-
-	Nodo actual; //nodo en el que se encuentra el pokemon adversario
-	Double energia; //energia que tiene el pokemon adversario
-	Boolean vivo; //indica si el pokemon esta vivo o no
-	
-}

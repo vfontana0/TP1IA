284f490052b39bc61d95d1ca674973c88c8c9ec7
Valentin Fontana <valefontana15@gmail.com>
2023-04-26 19:09:25 -0300

    cositas 2

--- src/domain/Pokemon.java
+++ src/domain/Pokemon.java
@@ -5,6 +5,7 @@ public class Pokemon {
 	Nodo actual; //nodo en el que se encuentra el pokemon adversario
 	Double energia; //energia que tiene el pokemon adversario
 	Boolean esMaestro;
+	Integer ciclosParaMoverse;
 	Boolean vivo; //indica si el pokemon esta vivo o no
 	public Nodo getActual() {
 		return actual;
@@ -30,6 +31,13 @@ public class Pokemon {
 	public void setEsMaestro(Boolean esMaestro) {
 		this.esMaestro = esMaestro;
 	}
+	public Integer getCiclosParaMoverse() {
+		return ciclosParaMoverse;
+	}
+	public void setCiclosParaMoverse(Integer ciclosParaMoverse) {
+		this.ciclosParaMoverse = ciclosParaMoverse;
+	}
+	
 	
 	
 

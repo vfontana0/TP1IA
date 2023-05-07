51440d60519eda51ab22bed3817c6926b8fc3dba
Tomas Assenza <tomyassenza@gmail.com>
2023-05-07 02:38:34 -0300

    Agregados archivos de interfaz

---
+++ src/com/utn/pokemonunite/Posiciones.java
@@ -0,0 +1,99 @@
+package com.utn.pokemonunite;
+
+import javafx.util.Pair;
+
+public class Posiciones {
+    public Pair<Integer, Integer> nodo1 = new Pair<>(110, 220);
+    public Pair<Integer, Integer> nodo2 = new Pair<>(185, 290);
+    public Pair<Integer, Integer> nodo3 = new Pair<>(166, 385);
+    public Pair<Integer, Integer> nodo4 = new Pair<>(232, 530);
+    public Pair<Integer, Integer> nodo5 = new Pair<>(245, 686);
+    public Pair<Integer, Integer> nodo6 = new Pair<>(280, 616);
+    public Pair<Integer, Integer> nodo7 = new Pair<>(302, 547);
+    public Pair<Integer, Integer> nodo8 = new Pair<>(250, 395);
+    public Pair<Integer, Integer> nodo9 = new Pair<>(276, 253);
+    public Pair<Integer, Integer> nodo10 = new Pair<>(350, 118);
+    public Pair<Integer, Integer> nodo11 = new Pair<>(451, 263);
+    public Pair<Integer, Integer> nodo12 = new Pair<>(378, 398);
+    public Pair<Integer, Integer> nodo13 = new Pair<>(451, 410);
+    public Pair<Integer, Integer> nodo14 = new Pair<>(507, 120);
+    public Pair<Integer, Integer> nodo15 = new Pair<>(521, 187);
+    public Pair<Integer, Integer> nodo16 = new Pair<>(553, 378);
+    public Pair<Integer, Integer> nodo17 = new Pair<>(554, 617);
+    public Pair<Integer, Integer> nodo18 = new Pair<>(725, 615);
+    public Pair<Integer, Integer> nodo19 = new Pair<>(636, 359);
+    public Pair<Integer, Integer> nodo20 = new Pair<>(639, 166);
+    public Pair<Integer, Integer> nodo21 = new Pair<>(739, 347);
+    public Pair<Integer, Integer> nodo22 = new Pair<>(761, 144);
+    public Pair<Integer, Integer> nodo23 = new Pair<>(920, 125);
+    public Pair<Integer, Integer> nodo24 = new Pair<>(818, 224);
+    public Pair<Integer, Integer> nodo25 = new Pair<>(904, 299);
+    public Pair<Integer, Integer> nodo26 = new Pair<>(905, 511);
+    public Pair<Integer, Integer> nodo27 = new Pair<>(998, 665);
+    public Pair<Integer, Integer> nodo28 = new Pair<>(880, 595);
+    public Pair<Integer, Integer> nodo29 = new Pair<>(817, 512);
+    
+    public Pair<Integer, Integer> getNodoN(int n) {
+    	switch(n) {
+    	case 1:
+            return nodo1;
+        case 2:
+            return nodo2;
+        case 3:
+            return nodo3;
+        case 4:
+            return nodo4;
+        case 5:
+            return nodo5;
+        case 6:
+            return nodo6;
+        case 7:
+            return nodo7;
+        case 8:
+            return nodo8;
+        case 9:
+            return nodo9;
+        case 10:
+            return nodo10;
+        case 11:
+            return nodo11;
+        case 12:
+            return nodo12;
+        case 13:
+            return nodo13;
+        case 14:
+            return nodo14;
+        case 15:
+            return nodo15;
+        case 16:
+            return nodo16;
+        case 17:
+            return nodo17;
+        case 18:
+            return nodo18;
+        case 19:
+            return nodo19;
+        case 20:
+            return nodo20;
+        case 21:
+            return nodo21;
+        case 22:
+            return nodo22;
+        case 23:
+            return nodo23;
+        case 24:
+            return nodo24;
+        case 25:
+            return nodo25;
+        case 26:
+            return nodo26;
+        case 27:
+            return nodo27;
+        case 28:
+            return nodo28;
+        case 29:
+            return nodo29;
+    	}
+    	return null;
+    }
+}
